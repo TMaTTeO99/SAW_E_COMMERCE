b3ce0e709fbfbad11d32cd5865980e160910c9d6
matteo <matteo.torchia64@gmail.com>
2024-02-13 16:02:53 +0100

    sistemata loginbase

--- src/LoginModules/LoginBase.js
+++ src/LoginModules/LoginBase.js
@@ -2,7 +2,6 @@ import email from '../Images/email.png';
 import google from '../Images/google.png';
 import { Link} from 'react-router-dom'; 
 import logo from '../Images/logo1.png';
-import { useEffect, useState} from 'react';
 
 export function RightColumn({GoogleHandler, EmailPasswordhandler, ViewCreateAccount, CheckBack}) {
 
@@ -27,11 +26,9 @@ export function RightColumn({GoogleHandler, EmailPasswordhandler, ViewCreateAcco
 				<div id='ContainerLogDiv'>		
 					<div className='Login_class_1' onClick={GoogleHandler}>
 						<img src={google} className='Link_' />
-						<h3 className='h3'>Accedi con google</h3>
 					</div>		
 					<div className='Login_class_2' onClick={EmailPasswordhandler}>
 						<img src={email} className='Link_' />
-						<h3 className='h3'>Email e password</h3>
 					</div>						
 				</div>
 				<Link id='create' onClick={setViewFromDefult}>Crea un account</Link>
@@ -48,43 +45,12 @@ export function RightColumn({GoogleHandler, EmailPasswordhandler, ViewCreateAcco
 //modulo per renderizzare la parte sinistra della schermata
 export function LeftColumn({backBlurred}) {
 	
-	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-	useEffect(() => {
-		const handleResize = () => {
-		  setWindowWidth(window.innerWidth);
-		};
-	
-		window.addEventListener('resize', handleResize);
-	
-		// Pulizia dell'evento quando il componente viene smontato
-		return () => {
-		  window.removeEventListener('resize', handleResize);
-		};
-	}, []);
-	const soglia = 793;
 	return (
-		<>
-		{windowWidth > soglia ? 
-			
-			(<div className='divLogoImage' id={backBlurred ? 'blur-effect': 'null_ID'}>
-				<div className='DivContainerLeftColumn'>
-					<img className='logoHomeLogin' src={logo} alt='MyEcommerce'/>
-					<h2 id='h2_1'>MY_E_COMMERCE</h2>
-				</div>
+		<div className='divLogoImage' id={backBlurred ? 'blur-effect': 'null_ID'}>
+			<div className='DivContainerLeftColumn'>
+				<img className='logoHomeLogin' src={logo} alt='MyEcommerce'/>
+				<h2 id='h2_1'>MY_E_COMMERCE</h2>
 			</div>
-			):
-			(
-				<div className='divLogoImage' id={backBlurred ? 'blur-effect': 'null_ID'}>
-					<div className='DivContainerLeftColumn'>
-						<img className='logoHomeLogin' src={logo} alt='MyEcommerce'/>
-						<h2 id='h2_1'>MY_E_COMMERCE</h2>
-					</div>
-				</div>
-			)
-		
-		
-		}
-		</>
+		</div>
 	);
 }
